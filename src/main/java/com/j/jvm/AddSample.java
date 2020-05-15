c887b877a702013c67eaccef45066c2e674ae06c
itlucky <hongjie_it@163.com>
2020-05-15 16:16:47 +0800

    此项目用于日常编写的测试用例等各种code

---
+++ src/main/java/com/j/jvm/AddSample.java
@@ -0,0 +1,13 @@
+package main.java.com.j.jvm;
+
+/**
+ * @Author: itlucky
+ * @Date: 2020/5/15 3:17 下午
+ * @Description:
+ * @Version: V1.0
+ **/
+public class AddSample {
+
+
+
+}
---
+++ src/main/java/com/j/jvm/CpuReOrder.java
@@ -0,0 +1,34 @@
+package main.java.com.j.jvm;
+
+/**
+ * @Author: itlucky
+ * @Date: 2020/5/14 9:33 下午
+ * @Description: 指令重排
+ * @Version: V1.0
+ **/
+public class CpuReOrder {
+
+    private int a = 0;
+    private boolean flag = false;
+    public void write(){
+        System.out.println("write...");
+        a = 1; //1
+        flag = true;  //2
+    }
+
+    public void read(){
+        System.out.println("read...");
+        if(flag) {   //3
+            int i = a*a;  //4
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        final CpuReOrder cpuReOrder = new CpuReOrder();
+        //写线程
+        new Thread(()->cpuReOrder.write()).start();
+        //读线程
+        new Thread(()->cpuReOrder.read()).start();
+    }
+}
