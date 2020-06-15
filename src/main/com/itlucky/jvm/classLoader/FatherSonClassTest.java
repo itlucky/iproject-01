1a98b9b9c2eaf9f12f848f70897361a5c20e97b3
itlucky <hongjie_it@163.com>
2020-06-15 22:22:32 +0800

    类加载测试

---
+++ src/main/com/itlucky/jvm/classLoader/FatherSonClassTest.java
@@ -0,0 +1,52 @@
+package main.com.itlucky.jvm.classLoader;
+
+/**
+ * 类到了初始化阶段，用户定义的Java程序代码才真正开始执行。
+ * Java程序对类的使用方式可分为两种:主动使用与被动使用。一般来说只有当对类的首次主动
+ * 使用的时候才会导致类的初始化，所以主动使用又叫做类加载过程中"初始化”开始的时机。
+ * 那啥是主动使用呢?类的主动使用包括以下六种:
+ * 1、创建类的实例，也就是new的方式
+ * 2、访问某 个类或接口的静态变量，或者对该静态变量赋值(被final修饰除外 ，会放入常量池)
+ * 3、调用类的静态方法
+ * 4、反射(如Class.forName( "java.lang String" ))
+ * 5、初始化某个类的子类，则其父类也会被初始化
+ * 6、Java虚拟机启动时被标明为启动类的类， 还有就是Main方法的类会首先被初始化
+ * 最后注意一-点对于静态字段， 只有直接定义这个字段的类才会被初始化(执行静态代码块)。
+ */
+public class FatherSonClassTest {
+
+    public String test = "fatherSonTest";
+
+    static class Father{
+        Father(){
+            System.out.println("father 构造函数");
+        }
+        static String fatherName = "小头爸爸";
+        final static String test = "final static test";
+        static {
+            System.out.println("father 静态代码块");
+        }
+    }
+
+    static class Son extends Father{
+        Son(){
+            super();
+            System.out.println("son 构造函数");
+        }
+        static String sonName = "大头儿子";
+        static {
+            System.out.println("son 静态代码块");
+        }
+    }
+
+    /**
+     * final修饰的可以理解为是常量。
+     * 执行结果分析：（结合类的初始化过程和规则即可理解）
+     */
+    public static void main(String[] args) {
+//        System.out.println(Son.fatherName);  //1
+        System.out.println(Son.sonName); //2
+        System.out.println(Son.test);  //3
+        new Son();  //4
+    }
+}
---
+++ src/main/com/itlucky/jvm/classLoader/MyClassLoader.java
@@ -0,0 +1,84 @@
+package main.com.itlucky.jvm.classLoader;
+
+import java.io.*;
+
+/**
+ * @author itlucky
+ * @version 0.0.1
+ * @description jdk不建议重写loadClass()方法，避免打破双亲委派模型。
+ *              所以可以重写当父类都找不到Class的情况下的findClass()方法。
+ * @since 2020/6/7 11:19 上午
+ */
+public class MyClassLoader extends ClassLoader{
+
+    private String root;
+
+    public String getRoot() {
+        return root;
+    }
+
+    public void setRoot(String root) {
+        this.root = root;
+    }
+
+    protected Class<?> findClass(String name) throws ClassNotFoundException{
+        byte[] classData = loadClassData(name);
+        if(classData == null){
+            throw new ClassNotFoundException();
+        } else {
+            return defineClass(name,classData,0,classData.length);
+        }
+    }
+
+    /**
+     * 获取类的二进制数据
+     * @param className 类的全路径名
+     * @return
+     *
+     *   File.separatorChar : 文件路径的斜杠符号 /
+     */
+    private byte[] loadClassData(String className){
+        String fileName = root + File.separator
+                + className.replace('.',File.separatorChar) + ".class";
+
+        try {
+            InputStream ins = new FileInputStream(fileName);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int length = 0;
+            while ((length =ins.read(buffer)) != -1){
+                baos.write(buffer,0,length);
+            }
+            return baos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //通过断点执行可以查看执行过程
+    public static void main(String[] args) {
+
+        MyClassLoader classLoader = new MyClassLoader();
+        classLoader.setRoot("/Users/itlucky/proj/study/tmp");
+
+        Class<?> testClass = null;
+        try {
+            //这里执行的结果是MyClassLoader，这个是项目之外的一个class，通过双亲委派模型没有找到父类对应的classLoader,所以就用了这里自定义的MyClassLoader.
+//            testClass = classLoader.loadClass("com.itlucky.ConsumerApplication");
+            //这里执行的结果是AppClassLoader, 是当前项目下就有的，双亲委派模型。也证实了这里的MyClassLoader的父类就是AppClassLoader。
+            testClass = classLoader.loadClass("main.com.itlucky.jvm.classLoader.FatherSonClassTest");
+            Object obj = testClass.newInstance();
+            System.out.println(obj.getClass().getClassLoader());
+        } catch (ClassNotFoundException r){
+            r.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
